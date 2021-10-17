0ae2a51ea3e94b7d0e6ed85756986c5a1c61580c
DanielSalis <danielsalis98@gmail.com>
2021-10-17 09:03:01 -0300

    estilizando botões

--- front/src/Components/Button/styles.js
+++ front/src/Components/Button/styles.js
@@ -4,6 +4,18 @@ export const Container = styled.button`
     width: 70px;
     height: 40px;
     background-color: #F9A826;
-    color: white;
+    color: #fff;
     text-align: center;
+    box-shadow: 0px 1px 0px 0px #fff6af;
+	border-radius:6px;
+	border:1px solid #F9A826;
+	display:inline-block;
+	cursor:pointer;
+	font-weight:bold;
+	text-decoration:none;
+
+    &:active {
+	position:relative;
+	top:1px;
+}
 `;
