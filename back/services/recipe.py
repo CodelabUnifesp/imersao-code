83eac843cec7c650e8609f46135dbe5cc54f80cb
DanielSalis <danielsalis98@gmail.com>
2021-10-17 14:30:52 -0300

    retornando title no get by id

--- back/services/recipe.py
+++ back/services/recipe.py
@@ -19,6 +19,7 @@ def getRecipe(id):
     if recipe != None:
         response = {
             "id":recipe.id,
+            "title": recipe.title,
             "ingredients":recipe.ingredients,
             "preparation":recipe.preparation
         }
